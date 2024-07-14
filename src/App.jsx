02891d88076e6c4673117c61c07e91eee5dd6b8f
Ramesh Tudu <rameshtudu3@gmail.com>
2024-07-14 20:39:53 +0545

    designed the ui

--- src/App.jsx
+++ src/App.jsx
@@ -13,13 +13,15 @@ function App() {
   const dispatch = useDispatch();
   return (
     <>
-      <div className="gap-10">
-        <h1>Counter: {count}</h1>
-        <button onClick={() => dispatch(increment())} className="m-2">Increment</button>
-        <button onClick={() => dispatch(decrement())} className="m-2">Decrement</button>
-        <button onClick={() => dispatch(incrementByAmount(5))}>
+      <div className=" w-[500px] h-[400px] bg-slate-400 gap-10 mx-auto rounded-md">
+        <h1 className="pt-20 text-5xl bold">Counter: {count}</h1>
+        <div className=" flex mt-10 items-center justify-center space-around">
+        <button onClick={() => dispatch(increment())} className="m-2 w-30 text-white bg-slate-800 p-2 rounded-lg">Increment</button>
+        <button onClick={() => dispatch(decrement())} className="m-2 w-30 text-white bg-slate-800 p-2 rounded-lg">Decrement</button>
+        <button onClick={() => dispatch(incrementByAmount(5))} className="w-30 text-white bg-slate-800 p-2 rounded-lg">
           Increment by 5
         </button>
+        </div>
       </div>
     </>
   );
